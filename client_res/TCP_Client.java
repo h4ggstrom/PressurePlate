7a4b0fc813240eda6fbb2a694a244ad6eac98aed
LordCafeine <alexandre.ledard1@gmail.com>
2024-10-13 16:34:28 +0200

    Client v1

--- client_res/TCP_Client.java
+++ client_res/TCP_Client.java
@@ -1,27 +1,46 @@
 package client_res;
 import java.net.*;
 import java.io.*;
+import java.lang.Exception;
 public class TCP_Client {
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
 
-    public void start(int port) {
+    public void start(int port)throws Exception {
         serverSocket = new ServerSocket(port);
         clientSocket = serverSocket.accept();
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String greeting = in.readLine();
-            if ("hello server".equals(greeting)) {
-                out.println("hello client");
+        out.println("abcde123456");
+        String id = in.readLine();
+        if(id.equals("id ok")){
+             out.println("10ko");
+        String volume = in.readLine();
+        if(volume.equals("volume ok")){
+            out.println("donnee123456");
+        }
+        else{
+            out.println("fichier trop volumineux");
+           throw new Exception("Fichier trop volumineux");
+        }
+        String transfert = in.readLine();
+            if (transfert.equals("transfert ok")){
+            out.println("succces du transfert, fermeture...");
+            System.out.println("Succes transfert");}
+            else{
+                out.println("transfert error");
+                throw new Exception("Echec Transfert");
             }
-            else {
-                out.println("unrecognised greeting");
             }
-    }
-
-    public void stop() {
+        else{
+            out.println("ID error");
+            throw new Exception("ID Error");
+        }
+        }
+    
+    public void stop() throws IOException {
         in.close();
         out.close();
         clientSocket.close();
@@ -29,6 +48,12 @@ public class TCP_Client {
     }
     public static void main(String[] args) {
         TCP_Client server=new TCP_Client();
-        server.start(6666);
+        try {
+            server.start(6666);
+            System.out.println("server lancer");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
