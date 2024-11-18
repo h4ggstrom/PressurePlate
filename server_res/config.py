38aa45bc7f3149a92751be18f81695468b67ee9a
h4ggstrom <robin2angelis@gmail.com>
2024-11-18 11:42:08 +0100

    updated comments

--- server_res/config.py
+++ server_res/config.py
@@ -1,20 +1,20 @@
 """
-ce fichier sert à stocker les variables d'environnement du serveur de manière sécurisée
+This file is used to load the environment variables from the .env file.
 """
 
 import os
 from dotenv import load_dotenv
 
-# on charge le fichier .env
-load_dotenv(dotenv_path='C:\\Users\\robin\\Desktop\\rep\\PressurePlate\\.env')
+# loading the .env file
+load_dotenv()
 
-# définition des variables d'environnement
+# setting the environment variables
 SERVER_IP = os.getenv("SERVER_IP")
 SERVER_PORT =int(os.getenv("SERVER_PORT"))
 LOGIN = os.getenv("LOGIN")
 PW = os.getenv("PASSWORD")
 DBNAME = os.getenv("DBNAME")
 
-# Vérification des variables d'environnement
+# checking if all the variables are defined
 if not all([SERVER_IP, SERVER_PORT, LOGIN, PW, DBNAME]):
-    raise EnvironmentError("Une ou plusieurs variables d'environnement ne sont pas définies.")
+    raise EnvironmentError("one or more environment variables are not defined")
--- server_res/tcpServ.py
+++ server_res/tcpServ.py
@@ -4,45 +4,45 @@ import config as cfg
 def start_server() -> None:
     """fonction de test pour l'écoute d'un client sur le port 6666
     """
-    #variables internes
+    #internal variables
     data: str
     server_socket: socket.socket
     conn: socket.socket
     addr: str
     
     
-    # setup du socket serveur
+    # server socket setup
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((cfg.SERVER_IP, cfg.SERVER_PORT))
     server_socket.listen(1)
     print("Serveur en attente de connexion...")
 
-    # Accepte une connexion du client
+    # waiting for client connection
     conn, addr = server_socket.accept()
     print(f"Connexion établie avec {addr} \n En attente d'ID...")
 
-    # boucle de dialogue avec le client
+    # communication with client
     with conn:
 
         while True:
-            # Réception de l'ID 
+            # ID received
             # conn.sendall("ID ? (exit pour fermer la connexion)\n".encode())
-            data = conn.recv(1024).decode().strip()  # .strip() pour enlever les espaces superflus
+            data = conn.recv(1024).decode().strip()  # .strip() to remove the newline and space characters
             if not data:
-                print("Aucune donnée reçue, fermeture de la connexion.")
+                print("No data received, closing connection.")
                 break
-            print(f"ID reçu = '{data}'")
+            print(f"ID received = '{data}'")
             
-            # Vérification de l'ID
+            # ID check
             if data == "abcde123456":
                 conn.sendall("id ok\n".encode())
                 
-                # Réception de la taille du fichier
+                # Receveing the size of the file
                 data = conn.recv(1024).decode().strip()
                 if not data:
-                    print("Aucune donnée reçue, fermeture de la connexion.")
+                    print("No data received, closing connection.")
                     break
-                print(f"Taille reçue = '{data}'")
+                print(f"size received = '{data}'")
                 
                 if data == "10ko":
                     conn.sendall("volume ok\n".encode())
@@ -50,30 +50,30 @@ def start_server() -> None:
                     # Réception des données envoyées
                     data = conn.recv(1024).decode().strip()
                     if not data:
-                        print("Aucune donnée reçue, fermeture de la connexion.")
+                        print("No data received, closing connection.")
                         break
-                    print(f"Données reçues = '{data}'")
+                    print(f"data received = '{data}'")
                     
                     if data == "donnee123456":
                         conn.sendall("transfert ok\n".encode())
-                        print("Transfert réussi.")
+                        print("transfert successful.")
                     else:
                         conn.sendall("transfert error\n".encode())
-                        print("Erreur de transfert.")
+                        print("Tansfert error.")
                         break
                 else:
                     conn.sendall("fichier trop volumineux\n".encode())
-                    print("Fichier trop volumineux.")
+                    print("file too big.")
                     break
             elif data == "exit":
-                conn.sendall("fermeture de la connexion\n".encode())
-                print("Fermeture de la connexion demandée par le client.")
+                conn.sendall("closing connection\n".encode())
+                print("closing connection.")
                 break
             else:
                 conn.sendall("ID error\n".encode())
-                print("Erreur d'ID.")
+                print("ID Error.")
 
-    # Ferme le socket serveur
+    # closing socket
     server_socket.close()
 
 if __name__ == "__main__":
