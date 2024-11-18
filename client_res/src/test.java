e3f67affcd4635a370496f77578adad317101ae1
LordCafeine <alexandre.ledard1@gmail.com>
2024-11-18 13:38:28 +0100

    test xml/github

--- client_res/src/test.java
+++ client_res/src/test.java
@@ -5,6 +5,7 @@ public class test {
     public static void main(String[] args) {
         XMLContent xml_content = new XMLContent();
                  xml_content.addEmployees("e_00000006","Lachaise","Jean","Caissier");
+                 xml_content.addFournisseur("fourni000001", "XML Company", "5 adresse de là bas", "0134225614");
                 System.out.println(xml_content.getXMLContent());
     }
 }
--- client_res/src/xmlClass/XMLContent.java
+++ client_res/src/xmlClass/XMLContent.java
@@ -15,6 +15,10 @@ public class XMLContent {
         this.xml_String = this.xml_String + new XMLEmployees(e_id,e_nom,e_prenom,e_poste).GetXMLEmployees();
     }
 
+    public void addFournisseur(String f_id, String f_nom, String f_adresse, String f_num){
+        this.xml_String = this.xml_String + new XMLFournisseur(f_id, f_nom, f_adresse, f_num).GetXMLFournisseur();
+    }
+
     public String getXMLContent(){
         this.xml_String = xml_String + "</content>";
         return this.xml_String;
